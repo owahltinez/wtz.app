bd94699fd6aee3e654b1a67740e97ce135e25080
Oscar Wahltinez <oscar@wahltinez.org>
2020-11-21 18:15:49 +0000

    Handle routes in 404

--- build.py
+++ build.py
@@ -4,10 +4,12 @@ import shutil
 from pathlib import Path
 from string import Template
 
+
 def render(tpl_path: Path, **config) -> str:
-    with open(tpl_path, 'r') as fd:
+    with open(tpl_path, "r") as fd:
         tpl = Template(fd.read())
         return tpl.substitute(**config)
+
 
 if __name__ == "__main__":
 
@@ -21,11 +23,11 @@ if __name__ == "__main__":
         shutil.rmtree(public)
     public.mkdir()
 
-    # Publish the output under the /public folder
-    with open(public / "index.html", "w") as fd:
-        fd.write(render('index.tpl.html', **config))
+    # Publish the output under the /public folder as 404.html to catch all routes
+    with open(public / "404.html", "w") as fd:
+        fd.write(render("index.tpl.html", **config))
 
     # Publish our custom 404 page undet the /public folder
-    with open(public / "404.html", "w") as fd:
-        fd.write(render('404.tpl.html', **config))
+    with open(public / "not-found.html", "w") as fd:
+        fd.write(render("not-found.tpl.html", **config))
 
